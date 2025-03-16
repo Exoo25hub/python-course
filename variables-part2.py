31c08f0a2bc87cfa68e8ec41a96d944cbda4ece4
Mahdi Ali <Exoo25pro@gmail.com>
2025-03-16 18:24:16 +0500

    Create variables-part2.py

---
+++ variables-part2.py
@@ -0,0 +1,9 @@
+#*******************************
+#           Variables          * 
+#*******************************
+
+name = "Mehdi"
+age = 9
+isHuman = False
+knowspython = None
+print("I am, " + name + " I am " + age + " years old, " + "i am human = " + " False")
